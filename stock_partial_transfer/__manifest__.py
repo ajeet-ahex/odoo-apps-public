dd2bdb3e09d1443448866c6a3a1fca1878dd92c8
ajeet-ahex <syamcharanrandhi@gmail.com>
2026-04-21 12:07:36 +0530

    feat: add module banner and icon images to manifest

--- stock_partial_transfer/__manifest__.py
+++ stock_partial_transfer/__manifest__.py
@@ -28,6 +28,7 @@
     'data': [
         'views/stock_picking_views.xml',
     ],
+    'images': ['static/description/banner.png'],
     'installable': True,
     'application': False,
     'auto_install': False,
